5ad36214dbd865217430e5e4b5fc38d6ae9d8588
Jisoo <spritaa@gmail.com>
2023-09-24 00:05:13 +0900

    REFACTOR: 싱글게임 단어입력 DTO 형식 변경

--- back/src/main/java/com/ssafy/confidentIs/keytris/dto/StartResponse.java
+++ back/src/main/java/com/ssafy/confidentIs/keytris/dto/StartResponse.java
@@ -20,18 +20,16 @@ import lombok.extern.slf4j.Slf4j;
 public class StartResponse {
 
   private Timestamp startTime;
+
   StatusResponse statusResponse;
   WordListResponse wordListResponse;
 
-  public StartResponse startRoom(Room room, String targetWord, List<String> subWordList) {
+  public StartResponse startRoom(Room room, WordListResponse wResponse) {
     SinglePlayer player = room.getPlayerList().get(0);
     StatusResponse sResponse = new StatusResponse();
-    WordListResponse wResponse = new WordListResponse();
-//    log.info("response params, room:{},player:{}",room,player);
-    log.info("startResponse wordList:{}", wResponse.toString());
     return StartResponse.builder()
         .startTime(room.getStartTime())
-        .wordListResponse(wResponse.start(subWordList, targetWord, 0L))
+        .wordListResponse(wResponse)
         .statusResponse(sResponse.idStatus(player, room))
         .build();
   }
