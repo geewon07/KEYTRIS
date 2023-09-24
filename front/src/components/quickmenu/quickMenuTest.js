dfab9c332369794fb8e619f8a688f4e89c01e9d7
GardenJB <ohjb93@gmail.com>
2023-09-25 08:42:14 +0900

    STYLE : home, quickmenu 스타일 적용

--- front/src/components/quickmenu/quickMenuTest.js
+++ front/src/components/quickmenu/quickMenuTest.js
@@ -1,48 +1,81 @@
 import './quickMenu.css'
-import MuteIcon  from '../../assets/mute.svg'
-import {Modal} from '../modal/ModalTest'
+import MuteIcon from '../../assets/mute.svg'
+// import {Modal} from '../modal/ModalTest'
+import { Modal3 } from '../modal/Modal3';
+import { Modal4 } from '../modal/Modal4';
+import { Modal5 } from '../modal/Modal5';
 import { useState } from 'react';
 
 export const QuickMenu = () => {
-    const [modal, setModal] = useState(false);
-    const [title, setTitle] = useState(false);
-    const [desc, setDesc] = useState(false);
-    return (
-        <nav>
-            <div>
-                <button className='nav-button' onClick={() => {
-                    console.log('음소거')
-                    }}>
-                    <img src={MuteIcon}/>
-                </button>
-           </div>
-           <div>
-            <button className='nav-button' onClick={() => {
-                setModal(true)
-                setTitle('게임 코드를 복사하여 친구에게 공유해주세요!');
-                setDesc('invite');
-                }}>
-                초대
-            </button>
-           </div>
-           <div>
-            <button className='nav-button' onClick={() => {
-                setModal(true);
-                setTitle('가이드가 들어가는데 바뀐다.')
-                }}>
-                가이드
-            </button>
-           </div>
-           <div>
-            <button className='nav-button' onClick={() => {
-                setModal(true)
-                setTitle('여기는 소개모달이 들어간다.')
-                }}>
-                소개
-            </button>
-           </div>
-           {modal && <Modal modalShow={title} title={title} setModal={setModal} desc={title} />}
-        </nav>
-    );
-    
-  }; 
+  // const [modal, setModal] = useState(false);
+  // const [title, setTitle] = useState(false);
+  // const [desc, setDesc] = useState(false);
+
+  const [isModalOpen3, setIsModalOpen3] = useState(false);
+  const handleOpenModal3 = () => {
+    setIsModalOpen3(true);
+  };
+
+  const handleCloseModal3 = () => {
+    setIsModalOpen3(false);
+  };
+
+  const [isModalOpen4, setIsModalOpen4] = useState(false);
+  const handleOpenModal4 = () => {
+    setIsModalOpen4(true);
+  };
+
+  const handleCloseModal4 = () => {
+    setIsModalOpen4(false);
+  };
+
+  const [isModalOpen5, setIsModalOpen5] = useState(false);
+  const handleOpenModal5 = () => {
+    setIsModalOpen5(true);
+  };
+
+  const handleCloseModal5 = () => {
+    setIsModalOpen5(false);
+  };
+
+  return (
+    <nav>
+      <div>
+        <button className='nav-button' onClick={() => {
+          console.log('음소거')
+        }}>
+          <img src={MuteIcon} />
+        </button>
+      </div>
+      <div>
+        <button
+          className="nav-button"
+          onClick={handleOpenModal3}
+        >
+          초대
+        </button>
+        <Modal3 isOpen={isModalOpen3} onClose={handleCloseModal3} />
+      </div>
+      <div>
+        <button
+          className="nav-button"
+          onClick={handleOpenModal4}
+        >
+          가이드
+        </button>
+        <Modal4 isOpen={isModalOpen4} onClose={handleCloseModal4} />
+      </div>
+      <div>
+        <button
+          className="nav-button"
+          onClick={handleOpenModal5}
+        >
+          소개
+        </button>
+        <Modal5 isOpen={isModalOpen5} onClose={handleCloseModal5} />
+      </div>
+      {/* {modal && <Modal modalShow={title} title={title} setModal={setModal} desc={title} />} */}
+    </nav>
+  );
+
+}; 
