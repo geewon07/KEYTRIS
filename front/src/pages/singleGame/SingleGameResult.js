40a890f2ff18320bbf932edcf680dc3b6c267677
오정빈 <ohjb93@gmail.com>
2023-09-26 14:53:23 +0900

    Fix/fe mainpage 페이지, 사이드네브바 비율 수정, css 수정

--- front/src/pages/singleGame/SingleGameResult.js
+++ front/src/pages/singleGame/SingleGameResult.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Score from "../../components/list/Score";
 import TodayNew from "../../components/list/TodayNew";
+
 export const SingleGameResult = () => {
+
   return (
-    <div className="total">
+    <div className="total" >
       <Score></Score>
       <TodayNew></TodayNew>
     </div>
