4792cdac28294574b0bfa2a7db11fd6bd32f9c62
GardenJB <ohjb93@gmail.com>
2023-09-27 14:46:53 +0900

    STYLE : score.js 수정

--- front/src/components/list/Score.js
+++ front/src/components/list/Score.js
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Score.css";
 import { overGame, rankPlayer } from "../../api/singleGame/SingleGameApi.js";
 import { Button } from "../button/ButtonTest";
 
-function Score({ playerId, playerResultList }) {
+function Score() {
   const [overResponse, setOverResponse] = useState(null);
   const [rankList, setRankList] = useState([]);
   const [nickName, setNickName] = useState("");
@@ -13,47 +12,9 @@ function Score({ playerId, playerResultList }) {
 
 
   useEffect(() => {
-    setOverResponse({
-      recordList: [
-          {
-              nickname: "dfdf",
-              score: 10000
-          },
-          {
-              nickname: "dddd",
-              score: 3000
-          },
-          {
-              nickname: "dgdgd",
-              score: 400
-          },
-          {
-              nickname: "ㅎㅇㅎㅇ",
-              score: 30
-          },
-          {
-              "nickname": "ㅎㅎㅎㅎ",
-              "score": 40
-          }
-      ],
-      statusResponse: {
-          playerId: "8b16bae2-87ee-4c8a-95dc-32ee7f62986e",
-          playerStatus: "OVER",
-          roomId: "891cd6fd-e7f2-4b67-a14c-80704c2eb59c",
-          roomStatus: "FINISHED"
-      },
-      record: true
-  })
-    // handleOverGame(overRequestDto);
+    handleOverGame(overRequestDto);
      // eslint-disable-next-line
   }, []);
-
-  useEffect(() => {
-    if(overResponse !== null) {
-    setRankList(overResponse.recordList);
-    }
-
-  }, [overResponse]);
 
   const navigate = useNavigate();
 
@@ -124,30 +85,26 @@ function Score({ playerId, playerResultList }) {
       <div className="my-score-display">
         <div className="list-title">점수</div>
         <hr />
-        {overResponse && <div className="rank-list" style={{ lineHeight: "1.5rem", wordSpacing: '1.5rem', }} >{listing}</div>}
+        {overResponse && <div className="rank-list">{listing}</div>}
         {/* 내 점수 */}
-        <div className="record" style={{ lineHeight: "2rem", marginTop: '1rem', marginBottom: '1rem', fontSize: '1.5rem', color: '#CCFF00', WebkitTextStroke: 'initial', wordSpacing: '1.7rem', }}>
-          내 점수 &nbsp;{score}&nbsp;&nbsp;
-          {/* 닉네임 입력란 */}
-          {typeof overResponse?.record === "boolean" &&
-            overResponse.record &&
-            !isSaving && (
-              <>
-                <input
-                  className="score-nickname-input"
+        {typeof overResponse?.record === "boolean" &&
+          overResponse.record &&
+          !isSaving && (
+            <>
+              <div className="record">
+                내 점수 &nbsp;{score}&nbsp;&nbsp;
+                {/* 닉네임 입력란 */}
+                <input className="score-nickname-input"
                   type="text"
                   value={nickName}
                   onChange={handleNicknameChange}
                   onKeyPress={handleKeyPress}
                   placeholder="닉네임 입력"
                 />
-                <button className="score-save-btn" onClick={handleSaveClick}>
-                  저장
-                </button>
-              </>
-            )}
-        </div>
-        
+                <button className="score-save-btn" onClick={handleSaveClick}>저장</button>
+              </div>
+            </>
+          )}
         <Button
           label="다시하기"
           onClick={() => handleButtonClickToGO("/")}
@@ -155,7 +112,6 @@ function Score({ playerId, playerResultList }) {
       </div>
     </>
   );
-  
 }
 
 export default Score;
